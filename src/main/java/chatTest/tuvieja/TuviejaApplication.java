f46ca07b39600300203b258fc2be561cabd295c5
VRralf <vrralf@gmail.com>
2022-10-28 17:07:57 -0300

    first commit

---
+++ src/main/java/chatTest/tuvieja/TuviejaApplication.java
@@ -0,0 +1,13 @@
+package chatTest.tuvieja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuviejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuviejaApplication.class, args);
+	}
+
+}
